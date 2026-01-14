a84e163257065cf15872f35e70e2fa072a447db5
FonziD1 <georgievfonzi@gmail.com>
2026-01-14 15:44:04 +0200

    did some exercises on the Odin project

--- DOM_Manipulation/OdinProject Etch-a-Sketch/js.js
+++ DOM_Manipulation/OdinProject Etch-a-Sketch/js.js
@@ -1,11 +1,60 @@
-const squareconainer = document.querySelector('.square-container');
+const squareContainer = document.querySelector(".square-container");
+let squaresPerRow = 16;
+const gridBtn = document.getElementById("grid-selector");
+const clearBtn = document.getElementById("grid-clear");
 
-for (let i =0; i < 256; i++){
-    document.createElement('div');
-    squareconainer.appendChild(div);
-    div.classList.add('square');
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = '#000';
+function createGrid() {
+  for (let i = 0; i < squaresPerRow * squaresPerRow; i++) {
+    const square = document.createElement("div");
+
+    square.classList.add("square");
+    square.style.width = `${100 / squaresPerRow}%`;
+    square.style.height = `${100 / squaresPerRow}%`;
+    square.style.borderColor = "black";
+    square.style.opacity = "0.2";
+
+
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "#333";
+      square.style.opacity = parseFloat(square.style.opacity) + 0.2;
     });
 
+    square.addEventListener('click', () => {
+        square.style.backgroundColor = "darkGreen";
+        square.style.opacity = parseFloat(square.style.opacity) + 0.2;
+    })
+    console.log(i);
+    squareContainer.appendChild(square);
+
+   /* setInterval(() => {
+      square.style.backgroundColor = "white";
+    }, 5000);
+     */
+  }
+   
 }
+
+createGrid();
+
+function clearGrid() {
+  squareContainer.innerHTML = "";
+}
+
+gridBtn.addEventListener("click", () => {
+  let newSize = prompt("Enter new grid size (max: 100):");
+
+  if (newSize !== null && !isNaN(newSize) && newSize > 0 && newSize <= 100) {
+    squaresPerRow = parseInt(newSize);
+  } else {
+    alert("Invalid input. Please enter a number between 1 and 100.");
+    return;
+  }
+  clearGrid();
+
+  createGrid();
+});
+
+clearBtn.addEventListener('click', () => {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => square.style.backgroundColor = "white");
+})
